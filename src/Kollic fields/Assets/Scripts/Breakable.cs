af9d334c2109d2f1aca2a844912fe87c6cbee647
jane1825 <sinyana383@gmail.com>
2025-04-07 18:38:24 +0300

    new idol

--- src/Kollic fields/Assets/Scripts/Breakable.cs
+++ src/Kollic fields/Assets/Scripts/Breakable.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    [SerializeField] GameObject hitEffectPrefab;
     int toughness = 150;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Weapon weapon))
         {
-
             this.toughness -= weapon.Dmg;
+            if (hitEffectPrefab != null)
+                Instantiate(hitEffectPrefab, collision.contacts[0].point, Quaternion.identity);
             Broken();
         }
 
