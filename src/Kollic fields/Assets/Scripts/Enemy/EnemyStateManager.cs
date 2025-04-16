9cb13dc0375730c2eb4b9cecdfbc6de5c443692b
ivan <149932990+Dumpling-OLW@users.noreply.github.com>
2025-04-16 20:12:55 +0300

    anim flags

--- src/Kollic fields/Assets/Scripts/Enemy/EnemyStateManager.cs
+++ src/Kollic fields/Assets/Scripts/Enemy/EnemyStateManager.cs
@@ -6,14 +6,15 @@ using UnityEngine.AI;
 public class EnemyStateManager : MonoBehaviour
 {
     [SerializeField] public Animator animator;
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] public NavMeshAgent agent;
     [SerializeField] Transform player;
-    [SerializeField] Transform target;
+    //[SerializeField] Transform target;
 
     public AState currentState;
     public IdleState idleState = new IdleState();
     public WalkState walkState = new WalkState();
     public AttackState attackState = new AttackState();
+    //public DeadState deadState = new DeadState();
 
     public float walkSpeed;
     public float agroDistance;
@@ -44,10 +45,8 @@ public class EnemyStateManager : MonoBehaviour
         if (!player)
             player = FindAnyObjectByType<XROrigin>().transform;
 
-        //agent.SetDestination(player.position);
-
-        SetDistination(player);
-        agent.destination = target.position;
+        agent.SetDestination(player.position);
+        agent.destination = player.position;
         if (currentState != null)
             currentState.UpdateState(this);
     }
@@ -55,27 +54,29 @@ public class EnemyStateManager : MonoBehaviour
     {
         agent.speed = newSpeed;
     }
-    public void SetDistination(Transform newDestination)
+    //public void SetDistination(Transform newDestination)
+    //{
+    //    target = newDestination;
+    //}
+
+    public Vector3 CheckOnTargetRotation()
     {
-        target = newDestination;
+        return (player.position - transform.position);
+    }
+
+    public void RotateTowards(Vector3 direction) 
+    {
+        var q = Quaternion.LookRotation(CheckOnTargetRotation());
+        transform.rotation = Quaternion.Lerp(transform.rotation, q, 3f * Time.deltaTime);
     }
 
     public float CheckOnTarget()
     {
-        return (transform.position - target.position).magnitude;
+        return (transform.position - player.position).magnitude;
+    }
+   
+    
     }
 
-    /*public void CheckPlayerDistance()
-    {
-        if (currentState == attackState)
-        {
-            if (CheckOnTarget() > attackDistance)
-            {
-                SwitchState(walkState);
-                //enemyManager.animator.SetBool("is_Attacking", false);
-            }
-        }
 
-    }*/
-}
 
