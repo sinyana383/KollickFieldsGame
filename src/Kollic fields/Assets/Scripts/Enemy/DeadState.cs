ed7485570f9a6306ef95b9402223cde89ffd2989
Dumpling-OLW <149932990+Dumpling-OLW@users.noreply.github.com>
2025-05-04 19:35:21 +0300

    added dead state
    needs testing tho

--- src/Kollic fields/Assets/Scripts/Enemy/DeadState.cs
+++ src/Kollic fields/Assets/Scripts/Enemy/DeadState.cs
@@ -4,17 +4,18 @@ public class DeadState : AState
 {
     public override void EnterState(EnemyStateManager enemyManager)
     {
-        // начало анимации idle
+        
+        enemyManager.animator.SetBool("is_Dead", true);
+        enemyManager.animator.SetBool("is_Attacking", false);
+        enemyManager.animator.SetBool("is_Angry", false);
         Debug.Log("Dead Enetered");
         enemyManager.SetSpeed(0);
 
     }
     public override void ExitState(EnemyStateManager enemyManager)
     {
-        Debug.Log("Dead Exit????");
     }
     public override void UpdateState(EnemyStateManager enemyManager)
     {
-        // провалить его сквозь пол
     }
 }
