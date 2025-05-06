bd497c5ce7f0c9dad546699f99a46b57f9de05bd
sinyana383 <sinyana383@gmail.com>
2025-05-06 20:21:34 +0300

    damned animation on enemy works only one day needed

--- src/Kollic fields/Assets/Scripts/Enemy/DeadState.cs
+++ src/Kollic fields/Assets/Scripts/Enemy/DeadState.cs
@@ -5,11 +5,11 @@ public class DeadState : AState
     public override void EnterState(EnemyStateManager enemyManager)
     {
         
-        enemyManager.animator.SetBool("is_Dead", true);
-        enemyManager.animator.SetBool("is_Attacking", false);
-        enemyManager.animator.SetBool("is_Angry", false);
+        //enemyManager.animator.SetBool("is_Dead", true);
+        //enemyManager.animator.SetBool("is_Attacking", false);
+        //enemyManager.animator.SetBool("is_Angry", false);
         Debug.Log("Dead Enetered");
-        enemyManager.SetSpeed(0);
+        //enemyManager.SetSpeed(0);
 
     }
     public override void ExitState(EnemyStateManager enemyManager)
