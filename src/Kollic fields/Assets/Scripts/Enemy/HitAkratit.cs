1292e9ea8d510cc1da54851f134dc8c4468633fc
questez <>
2025-05-14 20:00:13 +0300

    blood effect added

--- src/Kollic fields/Assets/Scripts/Enemy/HitAkratit.cs
+++ src/Kollic fields/Assets/Scripts/Enemy/HitAkratit.cs
@@ -3,10 +3,13 @@ using UnityEngine.Events;
 
 public class HitAkratit : MonoBehaviour
 {
+    public GameObject hitEffectPrefab;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out Weapon weapon))
         {
+            if (hitEffectPrefab != null)
+                Instantiate(hitEffectPrefab, other.ClosestPoint(transform.position), Quaternion.identity);
             EventManager.Akratit.OnAkratitHit?.Invoke(weapon.dmg);
         }
     }
