8bcbc6cd635e0f0a53ba63034c23cd7e6f149d46
questez <>
2025-05-14 18:49:03 +0300

    idol done

--- src/Kollic fields/Assets/Scripts/Interactable/Breakable.cs
+++ src/Kollic fields/Assets/Scripts/Interactable/Breakable.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 
-public class Breakable : Interactable
+public class Breakable : MonoBehaviour
 {
     [SerializeField] GameObject hitEffectPrefab;
     int toughness = 150;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Breakable OnCollisionEnter");
+        if (collision.gameObject.TryGetComponent(out Weapon weapon))
+        {
+
+            this.toughness -= weapon.Dmg;
+            if (hitEffectPrefab != null)
+                Instantiate(hitEffectPrefab, collision.contacts[0].point, Quaternion.identity);
+            if (this.toughness <= 0)
+                Broken();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Breakable OnTriggerEnter");
         if (other.gameObject.TryGetComponent(out Weapon weapon))
         {
+            
             this.toughness -= weapon.Dmg;
             if (hitEffectPrefab != null)
                 Instantiate(hitEffectPrefab, other.ClosestPoint(transform.position), Quaternion.identity);
