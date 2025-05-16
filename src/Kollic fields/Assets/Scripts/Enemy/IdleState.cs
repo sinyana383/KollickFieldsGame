ee9ed835dcce01d305988cc81e27144dcedbd2e0
Sinitskaya Yana <sinyana383@gmail.com>
2025-05-16 18:37:21 +0300

    Patrol done

--- src/Kollic fields/Assets/Scripts/Enemy/IdleState.cs
+++ src/Kollic fields/Assets/Scripts/Enemy/IdleState.cs
@@ -5,6 +5,8 @@ public class IdleState : AState
     public override void EnterState(EnemyStateManager enemyStateManager)
     {
         enemyStateManager.SetAutoBreaking(true);
+        enemyStateManager.animator.SetBool("is_Attacking", false);
+        enemyStateManager.animator.SetBool("is_Angry", false);
     }
 
     public override void ExitState(EnemyStateManager enemyStateManager)
