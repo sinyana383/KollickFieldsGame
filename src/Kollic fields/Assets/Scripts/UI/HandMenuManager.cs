dc7ad75bb7a9d747620d5af19a9f6dc2c29d5d24
Sinitskaya Yana <sinyana383@gmail.com>
2025-05-21 19:26:01 +0300

    HandMenu almost done

--- src/Kollic fields/Assets/Scripts/UI/HandMenuManager.cs
+++ src/Kollic fields/Assets/Scripts/UI/HandMenuManager.cs
@@ -1,26 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.UI.BodyUI;
 
 public class HandMenuManager : MonoBehaviour
 {
+    [Header("Function buttons")]
     [SerializeField] private Button saveButton;
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button exitButton;
+    
+    [Header("Panel Buttons")]
     [SerializeField] private Button savePanelButton;
     [SerializeField] private Button controlsButton;
     [SerializeField] private Button settingsButton;
+    [SerializeField] private Button mainMenuPanelButton;
+    [SerializeField] private Button exitPanelButton;
     
+    [Header("Panels")]
     [SerializeField] private Transform savePanel;
     [SerializeField] private Transform controlsPanel;
     [SerializeField] private Transform settingsPanel;
+    [SerializeField] private Transform mainMenuPanel;
+    [SerializeField] private Transform exitPanel;
     
 
     private void Start()
     {
         saveButton.onClick.AddListener(() =>EventManager.Save.OnSaveAll?.Invoke());
         saveButton.onClick.AddListener(() => savePanel.gameObject.SetActive(false));
+        
         savePanelButton.onClick.AddListener(() => savePanel.gameObject.SetActive(true));
         controlsButton.onClick.AddListener(() => controlsPanel.gameObject.SetActive(true));
         settingsButton.onClick.AddListener(() => settingsPanel.gameObject.SetActive(true));
+        
+        mainMenuPanelButton.onClick.AddListener(() => mainMenuPanel.gameObject.SetActive(true));
+        mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene(0));
+        
+        exitPanelButton.onClick.AddListener(() => exitPanel.gameObject.SetActive(true));
+        exitButton.onClick.AddListener(() => Application.Quit());
     }
 }
